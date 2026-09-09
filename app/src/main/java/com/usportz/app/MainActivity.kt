3929fd74ac38013010cea933f777acefe8a5ad5e
hurricanes92xx-hub <hurricanes92xx@gmail.com>
2026-09-09 11:26:49 -0400

    Use hardened USportz home UI

--- app/src/main/java/com/usportz/app/MainActivity.kt
+++ app/src/main/java/com/usportz/app/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { USportzApp(store) }
+        setContent { USportzAppV2(store) }
     }
 }
