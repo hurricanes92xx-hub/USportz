5a759a79f6ed9c48a0ba8b29dcdd0e1e24bfcfa1
hurricanes92xx-hub <hurricanes92xx@gmail.com>
2026-09-09 08:22:48 -0400

    Fix league hub artwork typography

--- app/src/main/java/com/usportz/app/LeagueHubActivity.kt
+++ app/src/main/java/com/usportz/app/LeagueHubActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
 
 class LeagueHubActivity : ComponentActivity() {
