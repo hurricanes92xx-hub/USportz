ec2f9185287690d5e1f09f99686ed8f63b1c10c9
hurricanes92xx-hub <hurricanes92xx@gmail.com>
2026-09-10 14:04:25 -0400

    Launch restored polished sports dashboard

--- app/src/main/java/com/usportz/app/MainActivity.kt
+++ app/src/main/java/com/usportz/app/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.activity.ComponentActivity
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, RichSportsActivity::class.java))
+        startActivity(Intent(this, RichSportsV2Activity::class.java))
         finish()
     }
 }
