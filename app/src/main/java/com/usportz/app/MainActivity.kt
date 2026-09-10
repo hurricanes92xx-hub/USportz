1ddc68a363e5cf263187e1b5a36ebf599eb8d448
hurricanes92xx-hub <hurricanes92xx@gmail.com>
2026-09-10 09:02:44 -0400

    Launch crash-safe large-source UI

--- app/src/main/java/com/usportz/app/MainActivity.kt
+++ app/src/main/java/com/usportz/app/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { USportzAppV3(store) }
+        setContent { USportzStableApp(store) }
     }
 }
