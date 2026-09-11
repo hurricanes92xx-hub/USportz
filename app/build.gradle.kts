87355c4697c7ec699a40b7da6f49e8d397bc566b
hurricanes92xx-hub <hurricanes92xx@gmail.com>
2026-09-11 15:37:32 -0400

    Add SVG image decoding for sports logos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,5 @@ dependencies {
     implementation("androidx.security:security-crypto:1.1.0-alpha06")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
     implementation("io.coil-kt:coil-compose:2.7.0")
+    implementation("io.coil-kt:coil-svg:2.7.0")
 }
