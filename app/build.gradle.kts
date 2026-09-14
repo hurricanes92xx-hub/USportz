87a6f3326867bf1ddcf7fcf4545bc120a98c5610
hurricanes92xx-hub <hurricanes92xx@gmail.com>
2026-09-14 13:11:28 -0400

    Remove unstable baseline profile plugin from app build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
-    id("androidx.baselineprofile")
 }
 
 android {
@@ -33,8 +32,6 @@ android {
     kotlinOptions { jvmTarget = "17" }
 }
 
-baselineProfile { automaticGenerationDuringBuild = false }
-
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2025.01.00"))
     implementation("androidx.activity:activity-compose:1.10.0")
@@ -54,5 +51,4 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
     implementation("io.coil-kt:coil-compose:2.7.0")
     implementation("io.coil-kt:coil-svg:2.7.0")
-    baselineProfile(project(":baselineprofile"))
 }
