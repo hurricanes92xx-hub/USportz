be7fdb25773dc43952f645ad946e197722b31ef4
hurricanes92xx-hub <hurricanes92xx@gmail.com>
2026-09-14 12:08:51 -0400

    Fix Baseline Profile target project configuration

--- baselineprofile/build.gradle.kts
+++ baselineprofile/build.gradle.kts
@@ -7,16 +7,13 @@ plugins {
 android {
     namespace = "com.usportz.app.baselineprofile"
     compileSdk = 35
+    targetProjectPath = ":app"
 
     defaultConfig {
         minSdk = 28
         targetSdk = 35
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-}
-
-baselineProfile {
-    targetProjectPath = ":app"
 }
 
 dependencies {
